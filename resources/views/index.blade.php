84970ab84e581580869d695a952de6fe1cd5aba9
Faiz Muzaki <faismuzaki25@gmail.com>
2020-10-28 13:53:58 +0700

    commit 3

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,21 +12,27 @@
   </head>
   <body>
 	  	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  		<conta
+    <div class="container">
 	  <a class="navbar-brand" href="#">Navbar</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
 	  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 	    <div class="navbar-nav">
-	      <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-	      <a class="nav-link" href="#">Features</a>
-	      <a class="nav-link" href="#">Pricing</a>
-	      <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+	      <a class="nav-link active" href="/">Home</a>
+	      <a class="nav-link" href="/about">About</a>
+	      <a class="nav-link" href="/mahasiswa">Mahasiswa</a>
 	    </div>
 	  </div>
+    </div>
 	</nav>
-    <h1>Hello, world!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+      <h1 class="mt-2">Hello, world!</h1>
+      </div>
+    </div>
+  </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
